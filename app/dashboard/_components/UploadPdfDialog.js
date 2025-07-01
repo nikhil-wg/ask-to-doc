c2f92cce1440e6ec134a958d4948c2811e0b0a3f
nikhil <nikwagh1609@gmail.com>
2025-07-01 07:51:52 +0530

    add upload file on database

--- app/dashboard/_components/UploadPdfDialog.js
+++ app/dashboard/_components/UploadPdfDialog.js
@@ -3,23 +3,45 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-function UploadPdfDialog({children}) {
+import { DialogClose } from "@radix-ui/react-dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+function UploadPdfDialog({ children }) {
   return (
     <div>
       <Dialog>
         <DialogTrigger asChild>{children}</DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle>Upload Pdf File</DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <div className="">
+                <p className="mt-5 text-black">Select the file to Upload</p>
+                <div className="gap-2 rounded-md border mt-1">
+                  <Input type="file" accept="application/pdf" className="" />
+                </div>
+                <div className="mt-2">
+                  <label className="text-black"> File Name:</label>
+                  <Input placeholder="File Name" className="text-black" />
+                </div>
+              </div>
             </DialogDescription>
           </DialogHeader>
+          
+
+          <DialogFooter className="sm:justify-end">
+            <DialogClose asChild>
+              <Button type="button" variant="secondary" className="ml-1">
+                Close
+              </Button>
+            </DialogClose>
+            <Button>Upload</Button>
+          </DialogFooter>
         </DialogContent>
       </Dialog>
     </div>
--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -13,6 +13,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as Storage from "../Storage.js";
 import type * as user from "../user.js";
 
 /**
@@ -24,6 +25,7 @@ import type * as user from "../user.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  Storage: typeof Storage;
   user: typeof user;
 }>;
 export declare const api: FilterApi<
